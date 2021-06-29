a1171399f85b400ba354d15b0ad3adb4e046a43d
David Plassmann <dplassmann@ngpvan.com>
2021-06-29 13:30:21 -0400

    Add Action system, handle movement manually

---
+++ TutorialRoguelike/TutorialRoguelike/Actions/MovementAction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace TutorialRoguelike.Actions
+{
+    class MovementAction : IAction
+    {
+        public Point Delta { get; private set; }
+        public MovementAction(int x, int y)
+        {
+            Delta = new Point(x, y);
+        }
+    }
+}
--- TutorialRoguelike/TutorialRoguelike/Program.cs
+++ TutorialRoguelike/TutorialRoguelike/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using SadConsole;
+using SadConsole.Entities;
 using SadConsole.Input;
 using SadRogue.Primitives;
+using TutorialRoguelike.Actions;
 using Console = SadConsole.Console;
 
 namespace TutorialRoguelike
@@ -17,15 +19,36 @@ namespace TutorialRoguelike
             Game.Create(Width, Height);
             Settings.WindowTitle = "Yet Another Roguelike Tutorial";
             Game.Instance.OnStart = Init;
+            Game.Instance.FrameUpdate += Instance_FrameUpdate;
             Game.Instance.Run();
             Game.Instance.Dispose();
+        }
+
+        private static void Instance_FrameUpdate(object sender, GameHost e)
+        {
+            var console = (Console)GameHost.Instance.Screen;
+            console.Clear();
+            console.Print(PlayerPosition.X, PlayerPosition.Y, "@");
         }
 
         private static void Init()
         {
             // Any startup code for your game. We will use an example console for now
             var startingConsole = (Console)GameHost.Instance.Screen;
-            startingConsole.Print(PlayerPosition.X, PlayerPosition.Y, "@");
+            startingConsole.IsFocused = true;
+            startingConsole.SadComponents.Add(new KeyboardHandler());
+        }
+
+        public static void HandleAction(IAction action)
+        {
+            if (action is EscapeAction)
+            {
+                Game.Instance.MonoGameInstance.Exit();
+            }
+            if (action is MovementAction move)
+            {
+                PlayerPosition = PlayerPosition.Add(move.Delta);
+            }
         }
     }
 }
