6d18d0016cf130b7f3206cfa2e2aaa00a5b9a7e9
David Plassmann <dplassmann@ngpvan.com>
2021-06-29 11:41:10 -0400

    Add basic solution. Print @

---
+++ TutorialRoguelike/TutorialRoguelike/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SadConsole;
+using SadConsole.Input;
+using SadRogue.Primitives;
+using Console = SadConsole.Console;
+
+namespace TutorialRoguelike
+{
+    class Program
+    {
+        public const int Width = 80;
+        public const int Height = 50;
+        public static Point PlayerPosition = new Point(Width / 2, Height / 2);
+
+        static void Main(string[] args)
+        {
+            Game.Create(Width, Height);
+            Settings.WindowTitle = "Yet Another Roguelike Tutorial";
+            Game.Instance.OnStart = Init;
+            Game.Instance.Run();
+            Game.Instance.Dispose();
+        }
+
+        private static void Init()
+        {
+            // Any startup code for your game. We will use an example console for now
+            var startingConsole = (Console)GameHost.Instance.Screen;
+            startingConsole.Print(PlayerPosition.X, PlayerPosition.Y, "@");
+        }
+    }
+}
