5e30b6aa13fdfc01ac128b9e278d5406e6c38d3e
David Plassmann <dplassmann@ngpvan.com>
2021-07-04 15:12:17 -0400

    Use map gen to add test wall

--- TutorialRoguelike.GoRogue/DungeonMap.cs
+++ TutorialRoguelike.GoRogue/DungeonMap.cs
@@ -1,4 +1,5 @@
-﻿using GoRogue.MapGeneration;
+﻿using System.Collections.Generic;
+using GoRogue.MapGeneration;
 using SadRogue.Integration;
 using SadRogue.Integration.Maps;
 using SadRogue.Primitives;
@@ -8,20 +9,41 @@ namespace TutorialRoguelike.GoRogue
 {
     public class DungeonMap : RogueLikeMap
     {
+        private const string WallFloor = "WallFloor";
+
         public DungeonMap(Point dungeonSize, Point mapSize) : base(dungeonSize.X, dungeonSize.Y, 4, Distance.Euclidean, viewSize: mapSize) {
             var generator = new Generator(dungeonSize.X, dungeonSize.Y)
                 .ConfigAndGenerateSafe(gen =>
                 {
                     gen.AddSteps(DefaultAlgorithms.RectangleMapSteps());
+                    gen.AddSteps(new TestWallGenerationStep());
                 });
 
-            var generatedMap = generator.Context.GetFirst<ISettableGridView<bool>>("WallFloor");
+            var generatedMap = generator.Context.GetFirst<ISettableGridView<bool>>(WallFloor);
 
             foreach (var location in this.Positions())
             {
                 bool walkable = generatedMap[location];
                 Color background = walkable ? Colors.Floor : Colors.Wall;
                 SetTerrain(new RogueLikeCell(location, Color.White, background, ' ', 0, walkable, walkable));
+            }
+        }
+        private class TestWallGenerationStep : GenerationStep
+        {
+            public TestWallGenerationStep() : base(null, (typeof(ISettableGridView<bool>), WallFloor))
+            {
+            }
+
+            protected override IEnumerator<object> OnPerform(GenerationContext context)
+            {
+                var wallFloor = context.GetFirst<ISettableGridView<bool>>(WallFloor);
+
+                foreach(var p in Rectangle.WithPositionAndSize((30, 22), 3, 1).Positions())
+                {
+                    wallFloor[p] = false;
+                }
+
+                yield return null;
             }
         }
     }
