3de5ad9bdb4f771392507241eda891d7e8474d7b
David Plassmann <dplassmann@ngpvan.com>
2021-07-04 11:24:02 -0400

    Clean usings

--- TutorialRoguelike.Manual/Entities/Entity.cs
+++ TutorialRoguelike.Manual/Entities/Entity.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using SadConsole;
+﻿using SadConsole;
 using SadRogue.Primitives;
 
 namespace TutorialRoguelike.Manual.Entities
