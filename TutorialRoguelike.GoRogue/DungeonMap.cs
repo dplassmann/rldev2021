6f8db812627cbe2fb8cbd579e2f9168c91dc5695
David Plassmann <dplassmann@ngpvan.com>
2021-07-07 12:26:29 -0400

    Convert bool to own types in map gen

--- TutorialRoguelike.GoRogue/DungeonMap.cs
+++ TutorialRoguelike.GoRogue/DungeonMap.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using GoRogue.MapGeneration;
 using SadRogue.Integration;
 using SadRogue.Integration.Maps;
@@ -16,30 +17,51 @@ namespace TutorialRoguelike.GoRogue
                 .ConfigAndGenerateSafe(gen =>
                 {
                     gen.AddSteps(DefaultAlgorithms.RectangleMapSteps());
+                    gen.AddSteps(new TileTypeStep());
                     gen.AddSteps(new TestWallGenerationStep());
                 });
 
-            var generatedMap = generator.Context.GetFirst<ISettableGridView<bool>>(WallFloor);
+            var generatedMap = generator.Context.GetFirst<ISettableGridView<TileTypes>>(WallFloor);
 
             foreach (var location in this.Positions())
             {
-                SetTerrain(TileFactory.Get(generatedMap[location] ? TileTypes.Floor : TileTypes.Wall, location));
+                SetTerrain(TileFactory.Get(generatedMap[location], location));
             }
         }
 
-        private class TestWallGenerationStep : GenerationStep
+        private class TileTypeStep : GenerationStep
         {
-            public TestWallGenerationStep() : base(null, (typeof(ISettableGridView<bool>), WallFloor))
+            public TileTypeStep() : base(null, (typeof(ISettableGridView<bool>), WallFloor))
             {
             }
 
             protected override IEnumerator<object> OnPerform(GenerationContext context)
             {
-                var wallFloor = context.GetFirst<ISettableGridView<bool>>(WallFloor);
+                var walkability = context.GetFirst<ArrayView<bool>>(WallFloor);
+                context.Remove(walkability);
+
+                var tileArray = walkability.ToArray().Select(p => p ? TileTypes.Floor : TileTypes.Wall).ToArray();
+                var wallFloor = new ArrayView<TileTypes>(tileArray, walkability.Width);
+
+                context.Add(wallFloor, WallFloor);
+
+                yield return null;
+            }
+        }
+
+        private class TestWallGenerationStep : GenerationStep
+        {
+            public TestWallGenerationStep() : base(null, (typeof(ISettableGridView<TileTypes>), WallFloor))
+            {
+            }
+
+            protected override IEnumerator<object> OnPerform(GenerationContext context)
+            {
+                var wallFloor = context.GetFirst<ISettableGridView<TileTypes>>(WallFloor);
 
                 foreach(var p in Rectangle.WithPositionAndSize((30, 22), 3, 1).Positions())
                 {
-                    wallFloor[p] = false;
+                    wallFloor[p] = TileTypes.Wall;
                 }
 
                 yield return null;
