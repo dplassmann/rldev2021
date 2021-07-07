acf8f2dc521762b394e14a332de2d71ef673d5da
David Plassmann <dplassmann@ngpvan.com>
2021-07-07 11:58:30 -0400

    Tweak colors

--- TutorialRoguelike.Manual/TileFactory.cs
+++ TutorialRoguelike.Manual/TileFactory.cs
@@ -5,7 +5,7 @@ namespace TutorialRoguelike.Manual
 {
     public class TileFactory
     {
-        public static Tile Floor => new Tile(true, true, new ColoredGlyph(Color.White, Colors.Floor, 0), new ColoredGlyph(Color.White, Colors.Floor, ' '));
-        public static Tile Wall => new Tile(false, false, new ColoredGlyph(Color.White, Colors.Wall, 826), new ColoredGlyph(Color.White, Colors.Wall, ' '));
+        public static Tile Floor => new Tile(true, true, new ColoredGlyph(Colors.Floor, Colors.Background, 0), new ColoredGlyph(Colors.Floor, Colors.Background, ' '));
+        public static Tile Wall => new Tile(false, false, new ColoredGlyph(Colors.Wall, Colors.Background, 826), new ColoredGlyph(Colors.Wall, Colors.Background, ' '));
     }
 }
